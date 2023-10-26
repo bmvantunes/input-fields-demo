01baa24c7a6dec43b1c31705448e54f0560bea08
Bruno Antunes <johndoe@example.com>
2023-10-26 21:02:37 +0100

    submit with data using name property

---
+++ apps/inputs-app/app/3/page.tsx
@@ -0,0 +1,40 @@
+'use client';
+
+export default function Index() {
+  return (
+    <form
+      onSubmit={(evt) => {
+        evt.preventDefault();
+        const formData = new FormData(evt.currentTarget);
+        const data = Object.fromEntries(formData);
+        console.log('Submitted Data', data);
+      }}
+    >
+      <p>Add onSubmit and show no data, because no name on inputs</p>
+
+      <fieldset>
+        <label htmlFor="first-name">First Name</label>
+        <input id="first-name" name="firstName" />
+      </fieldset>
+
+      <fieldset>
+        <label htmlFor="last-name">Last Name</label>
+        <input id="last-name" name="lastName" />
+      </fieldset>
+
+      <fieldset>
+        <label htmlFor="age">age</label>
+        <input id="age" name="age" />
+      </fieldset>
+
+      <fieldset>
+        <label>
+          Extra Input to Show example without IDs
+          <input name="extra" />
+        </label>
+      </fieldset>
+
+      <button type="submit">Create User</button>
+    </form>
+  );
+}
