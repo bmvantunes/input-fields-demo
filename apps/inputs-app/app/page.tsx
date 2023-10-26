1ade39ae8a3d71ab94ccbaae89e9fba707bb31ec
Bruno Antunes <johndoe@example.com>
2023-10-26 15:59:30 +0100

    Initial Commit

---
+++ apps/inputs-app/app/page.tsx
@@ -0,0 +1,23 @@
+
+export default function Index() {
+  return (
+    <form>
+      <fieldset>
+        <label>First Name</label>
+        <input />
+      </fieldset>
+
+      <fieldset>
+        <label>Last Name</label>
+        <input />
+      </fieldset>
+
+      <fieldset>
+        <label>Age</label>
+        <input />
+      </fieldset>
+
+      <button>Create User</button>
+    </form>
+  );
+}
---
+++ jest.preset.js
@@ -0,0 +1,7 @@
+const nxPreset = require('@nx/jest/preset').default;
+const path = require('path');
+
+module.exports = {
+  ...nxPreset,
+  setupFilesAfterEnv: [path.resolve(__dirname, 'setupFilesAfterEnv.ts')],
+};
